df06dff1ab9428fe03d747d40ef674701f735b10
deva <devanandu96@gmail.com>
2017-06-29 22:54:09 +0530

    my blog2

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls,name='admin'),
     url(r'',include('blog.urls')),
     url(r'^accounts/login/$', views.login, name='login'),
 ]
